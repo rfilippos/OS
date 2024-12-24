0e44711234cc168e8a233f4c86e89fd2bcc65880
rfilippos <rfilippos@ece.auth.gr>
2024-12-24 14:34:29 +0200

    hash: gets input, has default value

--- GitHashingExercise/hash_script.sh
+++ GitHashingExercise/hash_script.sh
@@ -1,9 +1,18 @@
 #/bin/bash
 
-UniqueInt=0379
+#default value (last four digits of my AEM)
+DefaultInt=0379
+
+# Check if input was given
+if [ $# -gt 0 ]; then
+	UniqueInt="$1"
+else
+	UniqueInt="$DefaultInt"
+fi
+
 
 #convert int to string to hash it later
-UniqueString=`echo -n UniqueInt`
+UniqueString=`echo -n $UniqueInt`
 
 #Generate hash and save it 
 hash=`echo -n "$UniqueString" | sha256sum | awk '{print $1}'`
